785adbf1b9ce85ddf31910691cea12f525b52cb3
AitorLas <militancias@hotmail.es>
2022-12-12 14:37:39 +0000

    prueba para subir

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Dashboard form '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: "/", component: Dashboard}
+  ],
 });
 
 export default router;
