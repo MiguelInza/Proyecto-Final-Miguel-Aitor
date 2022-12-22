2718d6bba7ba7ab0d8313212674b971d40cde9eb
Miguel Inza <inza.miguel@gmail.com>
2022-12-22 23:00:28 +0100

    commit

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,11 +1,15 @@
 <template>
   <nav>
     <div class="emoji-and-logo">
-
       <div>
-        <button class="btn-music" @click.prevent="audio.isPlaying ? pause(audio) : play(audio)" v-for="audio in audios" :key="audio.id" >
-    {{ audio.isPlaying ? 'Pause' : 'Play' }} {{ audio.name }}
-  </button>
+        <button
+          class="btn-music"
+          @click.prevent="audio.isPlaying ? pause(audio) : play(audio)"
+          v-for="audio in audios"
+          :key="audio.id"
+        >
+          {{ audio.isPlaying ? "Pause" : "Play" }} {{ audio.name }}
+        </button>
       </div>
 
       <div>
@@ -53,19 +57,18 @@
 </template>
 
 <script>
-
-
-
 export default {
   data() {
     return {
       homeLink: false,
-      audios: [{
-      isPlaying: false,
-      file: new Audio('/src/images/Wham - Last Christmas.mp3'),
-      id: 'muscle-car',
-      name: 'Last Christmas',
-      }],
+      audios: [
+        {
+          isPlaying: false,
+          file: new Audio("/src/images/Wham - Last Christmas.mp3"),
+          id: "muscle-car",
+          name: "Christmas song",
+        },
+      ],
     };
   },
   methods: {
@@ -99,11 +102,10 @@ nav {
   list-style: none;
 }
 .btn-music {
-
-  margin: 4px;
+  margin: 4px 0px 4px 4px;
   background-color: rgb(216, 66, 6);
   color: white;
-  padding: 5px 10px;
+  padding: 5px 10px 5px 10px;
   border-radius: 14px;
   font-size: 12px;
   cursor: pointer;
@@ -111,24 +113,22 @@ nav {
 .btn-music:hover {
   background-color: white;
   color: rgb(216, 66, 6);
-transition: 0.4s;
-font-weight: bold;
-border-radius: 8px;
-
+  transition: 0.4s;
+  font-weight: bold;
+  border-radius: 8px;
 }
-
 
 .emoji-and-logo {
   display: flex;
   align-items: center;
-  margin-left: 70px;
+  margin-left: 40px;
 }
 .logo-emoji {
   width: 100px;
   margin-left: 20px;
 }
 .logo-img-main {
-  width: 280px;
+  width: 200px;
   margin-left: 200px;
 }
 .buttons {
@@ -203,18 +203,20 @@ border-radius: 8px;
   }
   .emoji-and-logo {
     margin-left: 20px;
+
   }
   .logo-emoji {
-    margin-left: 20px;
+    margin-left: 5px;
     width: 60px;
     margin-top: 5px;
   }
   .logo-img-main {
-    margin-left: 10px;
+    margin-left: 0px;
+    margin-right: 0px;
     width: 125px;
   }
   .button1 button {
-    margin: 0px 7px;
+    margin: 0px 2px;
     width: 70px;
     padding: 5px;
     font-size: 9px;
@@ -230,7 +232,7 @@ border-radius: 8px;
     transition: 0.5s;
   }
   .button2 button {
-    margin: 0px 7px;
+    margin: 0px 2px;
     width: 80px;
     padding: 5px;
     font-size: 9px;
@@ -247,8 +249,8 @@ border-radius: 8px;
   }
 
   .button3 button {
-    margin: 0px 7px;
-    width: 50px;
+    margin: 0px 2px;
+    width: 40px;
     padding: 5px;
     font-size: 9px;
     font-weight: bolder;
@@ -256,7 +258,7 @@ border-radius: 8px;
   }
   .button3 button:hover {
     margin: 0px 9px;
-    width: 65px;
+    width: 60px;
     padding: 6px;
     font-size: 10px;
     border-radius: 4px;
